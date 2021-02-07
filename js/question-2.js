d25450b5ed74f49ffad1fc6400f341e6bc1dbb29
mrh1992 <71317905+mrh1992@users.noreply.github.com>
2021-02-07 19:34:52 +0100

    added answers

---
+++ js/question-2.js
@@ -0,0 +1,27 @@
+// Question 2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+    
+    const response = await fetch(url);
+
+    const games = await response.json();
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+        console.log(games[i].name);
+
+        if (i === 8) {
+            break;
+        }
+    
+
+        resultsContainer.innerHTML += `<div class="result">${games[i.name]}</div>`;
+    }
+}
+
+getGames();
